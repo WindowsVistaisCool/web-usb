089472ceab8b468e9c042acd9dde52a321459d5f
Kyle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2021-03-02 20:45:18 -0500

    added cvendor

--- myscript.js
+++ myscript.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', event => {
   let button = document.getElementById('connect')
-
+  let cvendor = document.getElementById('changeID')
+  var VENDOR_ID = 0x0781
+  
+  cvendor.addEventListener('click', async() => {
+    var newID = window.prompt("enter new ID (---- format w/ no 0x):")
+    alert("changed vendor id to" + newID)
+    var newerID = parseInt(newID, 10)
+    VENDOR_ID = newerID
+  })
   button.addEventListener('click', async() => {
     let device
-    const VENDOR_ID = 0x0781
     
     try {
       device = await navigator.usb.requestDevice({
