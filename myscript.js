3e72f2567b04313416d2cbfa9ac774eebb9f7e82
Kyle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2021-03-04 09:42:22 -0500

    remove unused/coming soon functions

--- myscript.js
+++ myscript.js
@@ -1,46 +1,18 @@
 document.addEventListener('DOMContentLoaded', event => {
-  let button = document.getElementById('connect')
+  let connec = document.getElementById('connect')
   let show = document.getElementById('showID')
-  let cvendor = document.getElementById('changeID')
-  let data = document.getElementById('showData')
-  let connect = document.getElementById('connectOld')
   var vndrID = 0x0781
   
   show.addEventListener('click', async() => {
     alert("The current vendor ID is: " + vndrID)
   })
-  data.addEventListener('click', async() => {
-    alert("this feature is coming soon, srry")
-  })
-  cvendor.addEventListener('click', async() => {
-    alert("this feature doesn't work, sorry (i've tried many times to fix, but it doenst work, and I dont really know how to use hex)")
-  })
-  button.addEventListener('click', async() => {
+  connec.addEventListener('click', async() => {
     let device
     
     try {
       device = await navigator.usb.requestDevice({
         filters: [{
           vendorId: vndrID
-        }]
-      })
-
-      console.log('open')
-      await device.open()
-      console.log('opened:', device)
-    } catch (error) {
-      console.log(error)
-    }
-	  await device.close()
-  })
-  connect.addEventListener('click', async() => {
-    let device
-    const vid = 0x0781
-    
-    try {
-      device = await navigator.usb.requestDevice({
-        filters: [{
-          vendorId: vid
         }]
       })
 
